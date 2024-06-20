2b8b0da105ed939a162ca19180712556e27e73b1
Jlabianca01 <jlabianca@albany.edu>
2024-06-20 12:58:34 -0400

    Changed main, produced cleaned csv

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import pandas as pd
+
 from data_loading import load_taxi_data, load_bike_data_day, load_bike_data_hour
 from data_cleaning import clean_taxi_data, clean_bike_data_day, clean_bike_data_hour
 from eda import eda_taxi_data, eda_bike_data_day, eda_bike_data_hour
 from optimization import optimize_example
 from simulation import run_bike_simulation
 from visualization import plot_results, plot_day_data, plot_hour_data
-import pandas as pd
 
 def main():
     # Load and clean data
@@ -33,6 +34,11 @@ def main():
     # Plot bike sharing data
     plot_day_data(bike_data_day)
     plot_hour_data(bike_data_hour)
+
+    # Export cleaned data to CSV files for Tableau
+    taxi_data.to_csv('cleaned_taxi_data.csv', index=False)
+    bike_data_day.to_csv('cleaned_bike_data_day.csv', index=False)
+    bike_data_hour.to_csv('cleaned_bike_data_hour.csv', index=False)
 
 if __name__ == "__main__":
     main()
